7f39cfac3af7b38acefb04eb31699005052da0db
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-04-10 01:51:17 +0530

    added: percentage calculator;

---
+++ app.py
@@ -0,0 +1,6 @@
+firstValue = int(input('enter first value: '))
+secondValue = int(input('enter second value: '))
+
+percentChange = (secondValue * 100) / firstValue
+
+print('percent diff is: ', percentChange - 100, '%')
